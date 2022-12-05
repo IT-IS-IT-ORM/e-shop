f59bdde5239c6f1fc9180379d04d6a4c704fbaa5
YernarT <toktaryernar@gmail.com>
2022-12-05 23:46:58 +0600

    feat: :sparkles: 引入 context 系统

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,109 @@
 // React
-import React from 'react';
+import React, { createContext, useState } from 'react';
 // Router
 import { BrowserRouter } from 'react-router-dom';
+
+// Utils
+import { isFunction } from './utils';
 
 // Components
 import { Layout } from '@/components';
 
+import computerList from '@/data/computer.json';
+import phoneList from '@/data/phone.json';
+import otherList from '@/data/other.json';
+import commentList from '@/data/comment.json';
+
+const contextDefaultValue = {
+	computerList,
+	phoneList,
+	otherList,
+	commentList,
+	favortieList: [],
+};
+export const DataContext = createContext(contextDefaultValue);
+
 export default function App() {
+	const [data, setData] = useState(contextDefaultValue);
+
+	const contextSetters = {
+		setComputerList(setStateAction) {
+			if (isFunction(setStateAction)) {
+				setData(prevState => ({
+					...prevState,
+					computerList: setStateAction(prevState.computerList),
+				}));
+				return;
+			}
+
+			setData(prevState => ({
+				...prevState,
+				computerList: setStateAction,
+			}));
+		},
+		setPhoneList(setStateAction) {
+			if (isFunction(setStateAction)) {
+				setData(prevState => ({
+					...prevState,
+					phoneList: setStateAction(prevState.phoneList),
+				}));
+				return;
+			}
+
+			setData(prevState => ({
+				...prevState,
+				phoneList: setStateAction,
+			}));
+		},
+		setOtherList(setStateAction) {
+			if (isFunction(setStateAction)) {
+				setData(prevState => ({
+					...prevState,
+					otherList: setStateAction(prevState.otherList),
+				}));
+				return;
+			}
+
+			setData(prevState => ({
+				...prevState,
+				otherList: setStateAction,
+			}));
+		},
+		setCommentList(setStateAction) {
+			if (isFunction(setStateAction)) {
+				setData(prevState => ({
+					...prevState,
+					commentList: setStateAction(prevState.commentList),
+				}));
+				return;
+			}
+
+			setData(prevState => ({
+				...prevState,
+				commentList: setStateAction,
+			}));
+		},
+		setFavoriteList(setStateAction) {
+			if (isFunction(setStateAction)) {
+				setData(prevState => ({
+					...prevState,
+					favortieList: setStateAction(prevState.favortieList),
+				}));
+				return;
+			}
+
+			setData(prevState => ({
+				...prevState,
+				favortieList: setStateAction,
+			}));
+		},
+	};
+
 	return (
-		<BrowserRouter>
-			<Layout />
-		</BrowserRouter>
+		<DataContext.Provider value={{ ...data, ...contextSetters }}>
+			<BrowserRouter>
+				<Layout />
+			</BrowserRouter>
+		</DataContext.Provider>
 	);
 }
